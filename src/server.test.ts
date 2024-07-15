8e5133b54dc7a0b59bb1ab99f1bf3f34f75a103d
Jack Menzie <jack.menzie@outlook.com>
2024-07-15 09:56:44 +0100

    Backend: Initial WebSocket setup and tests

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,26 +1,37 @@
 import t from "tap";
 import { buildServer } from "./server";
 
-t.test("Server", async (t) => {
+t.test("Region System Metrics WebSocket", async (t) => {
   const server = buildServer();
+  await server.ready();
 
   t.teardown(() => {
     server.close();
   });
 
-  t.test("GET", async (t) => {
-    const response = await server.inject({
-      method: "GET",
-      url: "/",
+  t.test("Connects to websocket successfully", async () => {
+    const socket = await server.injectWS("/region-system-metrics-ws");
+    const clientMessage = {
+      regions: ["us-east"],
+    };
+
+    // Resolves promise when server receives the message
+    let resolvePromise: (value: any) => void;
+    const promise = new Promise((resolve) => {
+      resolvePromise = resolve;
     });
 
-    t.equal(response.statusCode, 200, "returns a status code of 200");
-    t.match(
-      {
-        hello: "world",
-      },
-      JSON.parse(response.body),
-      "returns the expected message"
-    );
+    socket.on("message", (data) => {
+      resolvePromise(data.toString());
+    });
+
+    socket.send(JSON.stringify(clientMessage));
+    const response = await promise;
+    const result = JSON.parse(response);
+    socket.terminate();
+
+    t.match(result, {
+      hello: "world",
+    });
   });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,53 @@
+import { fastifyWebsocket } from "@fastify/websocket";
 import fastify from "fastify";
 
 export function buildServer() {
   const server = fastify();
 
-  server.get("/", async (request, reply) => {
-    return { hello: "world" };
+  server.register(fastifyWebsocket, {
+    options: {
+      maxPayload: 1048576, // 1 MiB (1024 bytes * 1024 bytes)
+    },
+    errorHandler(error, socket, request, reply) {},
+  });
+
+  server.register(async function () {
+    server.route({
+      method: "GET",
+      url: "/region-system-metrics-ws",
+      handler: () => {},
+      wsHandler: (socket, request) => {
+        console.log("client connected to server");
+
+        socket.on("close", () => {
+          console.log("client disconnected from the server");
+        });
+
+        socket.on("message", async (clientMessage: string) => {
+          const receivedMessage = clientMessage.toString();
+          console.log("client sent message", receivedMessage);
+
+          let regions: string[] = [];
+
+          try {
+            regions = JSON.parse(receivedMessage).regions;
+          } catch (e) {
+            console.log("Error parsing client message", receivedMessage);
+            const errorMessage = {
+              error: "Bad Request",
+              message: "message must be an object with property 'regions'",
+            };
+
+            socket.send(JSON.stringify(errorMessage));
+          }
+
+          const serverMessage = {
+            hello: "world",
+          };
+          socket.send(JSON.stringify(serverMessage));
+        });
+      },
+    });
   });
 
   return server;
